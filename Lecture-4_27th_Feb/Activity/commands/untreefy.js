7a4b4d376f50877a3e69989d64f4e11d5bea7bb6
Jasbir <mr.jasbirsingh96@gmail.com>
2020-02-27 16:35:03 +0530

    added untreefy

---
+++ Lecture-4_27th_Feb/Activity/commands/untreefy.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let path = require("path");
+let uniqid = require("uniqid");
+module.exports.untreefy = function() {
+  console.log("untreefy command has been Called");
+  let src = arguments[0];
+  let dest = arguments[1];
+  let root = {};
+  untreefyFolder(src, dest, root);
+  console.log(root);
+  console.log("All files have been copied");
+};
+
+function untreefyFolder(src, dest, node) {
+  let ans = fs.lstatSync(src).isDirectory();
+  if (ans == false) {
+    let uniqueName = uniqid();
+    node.isFile = true;
+    node.name = path.basename(src);
+    node.newName = uniqueName;
+    //copy file from src to dest=> and rename them
+    fs.copyFileSync(src, path.join(dest, uniqueName));
+  } else {
+    node.isFile = false;
+    node.name = path.basename(src);
+    node.children = [];
+    let childrens = fs.readdirSync(src);
+    for (let i = 0; i < childrens.length; i++) {
+      let cChildObj = {};
+      let chPath = path.join(src, childrens[i]);
+      untreefyFolder(chPath, dest, cChildObj);
+      node.children.push(cChildObj);
+    }
+  }
+}
