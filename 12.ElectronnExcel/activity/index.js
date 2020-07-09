e745e9200ed82391b20379bb9cf23f87a4d6e4cd
Jasbir <mr.jasbirsingh96@gmail.com>
2020-07-09 18:40:55 +0530

    updated with styling

--- 12.ElectronnExcel/activity/index.js
+++ 12.ElectronnExcel/activity/index.js
@@ -5,11 +5,32 @@ const dialog = require("electron").remote.dialog;
 $(document).ready(
     function () {
         let db;
+        let lsc;
         $("#grid .cell").on("click", function () {
             let rid = Number($(this).attr("ri"));
             let cid = Number($(this).attr("ci"));
             let ciAdrr = String.fromCharCode(cid + 65);
             $("#address-container").val(ciAdrr + (rid + 1));
+            $("#formula-container").val(db[rid][cid].formula);
+            // get al the formating from the cellobject
+            // add selected class 
+// checks
+            if (!cellObject.bold) {
+                $("#bold").addClass("selected");
+            }
+
+            $(this).addClass("selected");
+            if (lsc && lsc != this)
+                $(lsc).removeClass("selected");
+            lsc = this;
+        })
+
+        $("#grid-container").on("scroll", function () {
+            let scrollX = $(this).scrollLeft();
+            let scrollY = $(this).scrollTop();
+            console.log(scrollX + " " + scrollY);
+            $("#top-left-cell ,#left-col").css("left", scrollX + "px");
+            $("#top-left-cell,#top-row").css("top", scrollY + "px");
         })
         $(".menu-items").on("click", function () {
             $(".menu-options-item").removeClass("selected");
@@ -33,7 +54,7 @@ $(document).ready(
         $("#Open").on("click", async function () {
             let odb = await dialog.showOpenDialog();
             let fp = odb.filePaths[0];
-            let content = await fs.promises.readFileSync(fp);
+            let content = await fs.promises.readFile(fp);
             db = JSON.parse(content);
             // loop 
             let rows = $("#grid").find(".row");
@@ -41,8 +62,17 @@ $(document).ready(
                 let cRowCells = $(rows[i]).find(".cell");
                 for (let j = 0; j < cRowCells.length; j++) {
                     // DB
+                    let cell = db[i][j];
+                    $(cRowCells[j]).html(cell.value);
+                    $(cRowCells[j]).css("font-family", cell.fontfamily);
+                    $(cRowCells[j]).css("font-size", cell.fontSize + "px");
+                    $(cRowCells[j]).css("font-weight", cell.bold ? "bolder" : "normal");
+                    $(cRowCells[j]).css("text-decoration", cell.underline ? "underline" : "none");
+                    $(cRowCells[j]).css("font-style", cell.italic ? "italic" : "normal");
+                    $(cRowCells[j]).css("background-color", cell.bgColor);
+                    $(cRowCells[j]).css("color", cell.textColor);
+                    $(cRowCells[j]).css("text-align", cell.halign);
 
-                    $(cRowCells[j]).html(db[i][j].value);
                 }
             }
         })
@@ -59,17 +89,90 @@ $(document).ready(
                         value: "",
                         formula: "",
                         downstream: [],
-                        upstream: []
-
+                        upstream: [],
+                        fontfamily: "Arial",
+                        fontSize: 12,
+                        bold: false,
+                        underline: false,
+                        italic: false,
+                        textColor: "#000000",
+                        bgColor: "white",
+                        halign: 'left'
                     }
+
                     row.push(cell);
+
                     // UI 
                     $(cRowCells[j]).html("");
+                    $(cRowCells[j]).css("font-family", cell.fontfamily);
+                    $(cRowCells[j]).css("font-size", cell.fontSize + "px");
+                    $(cRowCells[j]).css("font-weight", cell.bold ? "bolder" : "normal");
+                    $(cRowCells[j]).css("text-decoration", cell.underline ? "underline" : "none");
+                    $(cRowCells[j]).css("font-style", cell.italic ? "italic" : "normal");
+                    $(cRowCells[j]).css("background-color", cell.bgColor);
+                    $(cRowCells[j]).css("color", cell.textColor);
+                    $(cRowCells[j]).css("text-align", cell.halign);
                 }
                 db.push(row);
             }
             console.log(db);
+            // Home menu click 
+
         })
+        // *************Formatting*****
+
+        $("#bold").on("click", function () {
+            $(this).toggleClass("selected");
+            let isBold = $(this).hasClass("selected");
+            $("#grid .cell.selected").css("font-weight", isBold ? "bolder" : "normal");
+            let cellElem = $("#grid .cell.selected");
+            let { rowId, colId } = getRc(cellElem);
+            let cellObject = getCellObject(rowId, colId);
+            cellObject.bold = isBold;
+        })
+        $("#italic").on("click", function () {
+            $(this).toggleClass("selected");
+            let isitalic = $(this).hasClass("selected");
+
+            $("#grid .cell.selected").css("font-style", isitalic ? "italic" : "normal");
+            let cellElem = $("#grid .cell.selected");
+            let { rowId, colId } = getRc(cellElem);
+            let cellObject = getCellObject(rowId, colId);
+            cellObject.b = isitalic;
+        })
+        $("#underline").on("click", function () {
+            $(this).toggleClass("selected");
+            let isunderline = $(this).hasClass("selected");
+            $("#grid .cell.selected").css("text-decoration", isunderline ? "underline" : "none");
+            let cellElem = $("#grid .cell.selected");
+            let { rowId, colId } = getRc(cellElem);
+            let cellObject = getCellObject(rowId, colId);
+            cellObject.underline = isunderline;
+        })
+
+        $("#font-family").on("change", function () {
+            let fontFamily = $(this).val();
+            console.log(fontFamily);
+            $("#grid .cell.selected").css("font-family", fontFamily);
+            let cellElem = $("#grid .cell.selected");
+            let { rowId, colId } = getRc(cellElem);
+            let cellObject = getCellObject(rowId, colId);
+            cellObject.fontfamily = fontFamily;
+
+        })
+        $("#font-size").on("change", function () {
+            let fontSize = $(this).val();
+            console.log(fontSize);
+            $("#grid .cell.selected").css("font-size", fontSize + "px");
+            let cellElem = $("#grid .cell.selected");
+            let { rowId, colId } = getRc(cellElem);
+            let cellObject = getCellObject(rowId, colId);
+            cellObject.fontSize = fontSize;
+
+        })
+        // *************Formatting*****
+
+
         // formula Logic Starts Here
         // let lsc;
         $("#grid .cell").on("blur", function () {
@@ -90,6 +193,7 @@ $(document).ready(
             // console.log(db);
         })
         $("#formula-container").on("blur", function () {
+            // 
             let address = $("#address-container").val();
             // console.log(address);
             let { rowId, colId } = getRcFromAddress(address);
@@ -99,20 +203,15 @@ $(document).ready(
             if (cellObject.formula == $(this).val()) {
                 return;
             }
-
             if (cellObject.formula) {
                 removeFormula(cellObject, rowId, colId)
             }
-
             cellObject.formula = formula;
             let eValuatedVal = evaluate(cellObject);
             setUpFormula(rowId, colId, formula);
+            // value set
             updateCell(rowId, colId, eValuatedVal, cellObject);
         })
-
-
-
-
         function evaluate(cellObject) {
             let formula = cellObject.formula;
             // ( A1 + A2 )
@@ -125,6 +224,7 @@ $(document).ready(
                     let fParent = db[parentRc.rowId][parentRc.colId];
                     let value = fParent.value;
                     formula = formula.replace(formulaComponent[i], value)
+                    // (10 + 20)
                 }
             }
             // ( 10 + 20 )
@@ -136,7 +236,12 @@ $(document).ready(
         function updateCell(rowId, colId, val, cellObject) {
             // update yourself
             $(`#grid .cell[ri=${rowId}][ci=${colId}]`).html(val);
-            cellObject.value = val;
+            // val = val.trim();
+            cellObject.value = val
+            // if (val) {
+            // } {
+            //     cellObject.value = 0;
+            // }
             for (let i = 0; i < cellObject.downstream.length; i++) {
                 let sdsorc = cellObject.downstream[i];
                 let fdso = db[sdsorc.rowId][sdsorc.colId];
@@ -144,7 +249,6 @@ $(document).ready(
                 updateCell(sdsorc.rowId, sdsorc.colId, evaluatedValue, fdso);
             }
         }
-
         function setUpFormula(rowId, colId, formula) {
             // parent  downstream add
             let cellObject = getCellObject(rowId, colId);
@@ -205,6 +309,8 @@ $(document).ready(
         function init() {
             $("#File").trigger("click");
             $("#New").click();
+            $("#Home").click();
+            $("#grid .cell").eq(0).click();
         }
         init();
 
