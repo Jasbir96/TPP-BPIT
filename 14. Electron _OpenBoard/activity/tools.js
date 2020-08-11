9c520b58b2111e26feb7142b816cc734d8da11b2
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-11 16:53:44 +0530

    Class-2 BP-1 active and color logic

--- 14. Electron _OpenBoard/activity/tools.js
+++ 14. Electron _OpenBoard/activity/tools.js
@@ -1,9 +1,39 @@
-ctx.lineWidth=5;
+ctx.lineWidth = 5;
+ctx.lineCap="round";
+ctx.lineJoin='round';
+let activeTool = 'pencil';
+let pencil = document.querySelector("#pencil");
+let eraser = document.querySelector("#eraser");
+let pencilOptions = document.querySelector("#pencil-options");
+let eraserOptions = document.querySelector("#eraser-options");
 function handleTool(tool) {
     if (tool == "pencil") {
-        ctx.strokeStyle = "black";
+        if (activeTool == "pencil") {
+            pencilOptions.classList.add("show");
+        } else {
+            ctx.strokeStyle = "black";
+            activeTool = "pencil";
+            eraserOptions.classList.remove("show");
+        }
     } else if (tool == "eraser") {
-        ctx.strokeStyle = "white"
-    }
+        if (activeTool == "eraser") {
+            eraserOptions.classList.add("show");
+        } else {
+            ctx.strokeStyle = "white";
+            activeTool = "eraser";
+            pencilOptions.classList.remove("show");
+        }
 
+    }
+}
+function changeColor(color) {
+    ctx.strokeStyle = color;
+}
+
+let sliders = document.querySelectorAll("input[type='range']");
+for (let i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener("change", function () {
+        let width = sliders[i].value;
+        ctx.lineWidth = width;
+    })
 }
