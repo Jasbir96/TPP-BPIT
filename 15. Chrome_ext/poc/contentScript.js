8a266d39aa54dba7a8b08c151c518018fd858926
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-24 17:51:07 +0530

    updated with images

--- 15. Chrome_ext/poc/contentScript.js
+++ 15. Chrome_ext/poc/contentScript.js
@@ -1,1 +1,16 @@
-console.log("Mai content se ayaa");
+// console.log("Mai content se ayaa");
+// 
+let imgPaths = [
+    "images/img-1.jpg",
+    "images/img-2.jpg",
+    "images/img-3.jpg",
+    "images/img-4.jpg",
+    "images/img-5.jpeg"
+];
+let aIP = document.querySelectorAll("img");
+for (let i = 0; i < aIP.length; i++) {
+    let idx = Math.floor(Math.random() * imgPaths.length);
+    let fullPath = chrome.extension.getURL(imgPaths[idx]);
+    console.log(fullPath);
+    aIP[i].src = fullPath;
+}
