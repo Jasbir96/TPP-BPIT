8e7ebd7ae385d68f6fefcb79166bf9f22dfc74c1
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-25 17:39:16 +0530

    C-2 B-2 added popup logic

--- 15. Chrome_ext/social-media-blocker/background.js
+++ 15. Chrome_ext/social-media-blocker/background.js
@@ -1,1 +1,14 @@
-console.log("I am in background");
+let blockedSites = [];
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        blockedSites.push(request);
+        console.log(blockedSites)
+        // console.log(sender);
+        // if (request.greeting == "hello") {
+        //     console.log("Recieved from popup");
+        // }
+        sendResponse("Hello from content");
+        //  check every second
+        // relplaceImg()
+    })
--- 15. Chrome_ext/social-media-blocker/script.js
+++ 15. Chrome_ext/social-media-blocker/script.js
@@ -1,1 +1,35 @@
-console.log("Inside popup.js");
+// console.log("Inside popup.js");
+const button = document.querySelector(".btn");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+button.addEventListener("click", async function () {
+    let toBeBLocked = input.value;
+    if (toBeBLocked) {
+        // send Site to background
+        await sendTobackground(input.value);
+        let li = document.createElement("li");
+        li.setAttribute("class", "list-group-item");
+        li.innerHTML = `${toBeBLocked} <i class="fas fa-times"></i>`;
+        ul.appendChild(li);
+        let i = li.querySelector("i");
+        i.addEventListener("click", function () {
+            // let isRemoved = removefromdb(i.parentNode.textContent);
+            // console.log();
+            // if (isRemoved) {
+                // }
+                    i.parentNode.remove();
+        })
+        // send mesage to background
+        input.value = "";
+    }
+})
+function sendTobackground(message) {
+    return new Promise(function (resolve, reject) {
+        chrome.runtime.sendMessage(message, function (response) {
+            // console.log("recieved from background.js")
+            resolve(true);
+        });
+    })
+
+
+}
