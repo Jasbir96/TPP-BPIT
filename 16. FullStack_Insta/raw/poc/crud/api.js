6d779b104ef82456ae3f197a9549c9fd692ea1d7
Jasbir <mr.jasbirsingh96@gmail.com>
2020-08-31 18:51:50 +0530

    C-1 B-3 added post anf get

--- 16. FullStack_Insta/raw/poc/crud/api.js
+++ 16. FullStack_Insta/raw/poc/crud/api.js
@@ -10,9 +10,10 @@ const fs = require("fs");
 const path = require("path");
 // REST API
 // HTTP request => 
-// read  => GET
 // create => POST
+// http packet => body 
 app.use(express.json());
+// handler req.body 
 app.post("/api/users", function (req, res) {
     let user = req.body;
     // db Save
@@ -20,15 +21,53 @@ app.post("/api/users", function (req, res) {
     // if a new entry is created on server
     // memory -> ram
     userDB.push(user);
-    fs.writeFileSync(path.join(__dirname, "user.json"), JSON.stringify(userDB));
-//    res status code server send 
+    fs.writeFileSync(path.join(__dirname, 
+        "user.json"),
+        JSON.stringify(userDB));
+    //    res status code server send 
     res.status(201).json({
         success: "successfull",
         user: user
     })
 })
 
+// read  => GET ONE 
+app.get("/api/users/:user_id", function (req, res) {
+    let { user_id } = req.params;
+    let user;
+    for (let i = 0; i < userDB.length; i++) {
+        if (userDB[i].user_id == user_id) {
+            user = userDB[i];
+        }
+    }
+    res.status(200).json({
+        status: "success",
+        user: user != undefined ? user : "no user"
+    })
+})
 // update => PATCH
+// client will your id in url and data to update in req.body
+// api/users/12345
+app.patch("/api/users/:user_id", function (req, res) {
+    let { user_id } = req.params;
+    // {user_id:12345}
+    let user;
+    let toUpdate=req.body;
+    for (let i = 0; i < userDB.length; i++) {
+        if (userDB[i].user_id == user_id) {
+            user = userDB[i];
+        }
+    }
+    // update 
+    res.status(200).json({
+        status: "success",
+        user: user != undefined ? user : "no user"
+    })
+})
+// search and delete 
+app.delete("/api/users/:user_id", function (req, res) {
+
+})
 // delete=> DELETE 
 // localhost:3000/api/users
 app.listen(3000, function () {
