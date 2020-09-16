92dfb85e9e92e28d22b776db0f1a88901996a5a0
Jasbir <mr.jasbirsingh96@gmail.com>
2020-09-16 21:58:17 +0530

    added date logic

--- 16. FullStack_Insta/activity/model/postModel.js
+++ 16. FullStack_Insta/activity/model/postModel.js
@@ -1,5 +1,4 @@
 
 const { createEntityFact } = require("../utility/modelFactory");
 const create = createEntityFact("post");
-
 module.exports.create = create;
--- 16. FullStack_Insta/activity/utility/modelFactory.js
+++ 16. FullStack_Insta/activity/utility/modelFactory.js
@@ -1,11 +1,16 @@
 let { v4: uuidv4 } = require("uuid");
 let db = require("./connection");
 module.exports.createEntityFact = function (entity) {
-//  function
+    //  function
     return function (entityObj) {
         // create user in db
         // google / facebook 
         entityObj.id = uuidv4();
+        if (entity == "post") {
+            let date = new Date();
+            entityObj.created_at = date.toISOString().slice(0, 19).replace('T', ' ');
+
+        }
         // validation=> extra value 
         return new Promise(function (resolve, reject) {
             db.query(`INSERT INTO ${entity} SET?`, entityObj, function (err, result) {
