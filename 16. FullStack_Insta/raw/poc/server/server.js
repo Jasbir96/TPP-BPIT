602e4d77267415da35c533aa38becdc0dbabcc2c
Jasbir <mr.jasbirsingh96@gmail.com>
2020-10-07 17:50:00 +0530

    added oauth code

---
+++ 16. FullStack_Insta/raw/poc/server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+app.use(express.static("public"));
+
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+// req will be send through browser
+// start 
+passport.initialize();
+// setting define
+passport.use(new GoogleStrategy({
+    clientID: "id",
+    clientSecret: "PfloDXkHhqD4e2eNMQfeDAcu",
+    callbackURL: "http://localhost:4000/auth/callback"
+    // passReqToCallback: true
+},
+    function (request, accessToken, refreshToken, profile, done) {
+        console.log("Inside passport cb")
+        console.log(profile);
+        // signup
+        // login
+    }
+));
+// req to google server => to get email  and profile
+// passport .authectiecte => function google [email,profile],/setting
+// redirect to googl server
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ['email', 'profile'] }))
+// req from google server
+// authenticate=> 
+app.get("/auth/callback", passport.authenticate("google"), function (req, res) {
+    //identify 
+    // req using passport.authenticate
+    res.send("user authenticated");
+})
+app.listen(4000,
+    console.log("Server is listening at port 4000"));
