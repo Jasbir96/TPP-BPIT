4e6e51c771109b52867bf8e7237ad7e91c94cf83
Jasbir <mr.jasbirsingh96@gmail.com>
2020-10-07 18:56:28 +0530

    added user

--- 16. FullStack_Insta/raw/poc/server/server.js
+++ 16. FullStack_Insta/raw/poc/server/server.js
@@ -1,24 +1,87 @@
 const express = require("express");
 const app = express();
 const passport = require("passport");
+const util = require("util");
+var mysql = require('mysql')
+const { v4: uuidv4 } = require('uuid');
+const cookieSession = require("cookie-session");
+// cookie define
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["hello"]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+// 
+// ***************DataBase connection*******************************
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    // db password
+    password: 'password',
+    // db instance name 
+    database: 'insta_pp'
+})
+connection.connect();
+let query = util.promisify(connection.query).bind(connection);
+// ********************************************************************
+console.log("connected to db");
 app.use(express.static("public"));
-
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
 // req will be send through browser
 // start 
-passport.initialize();
+
 // setting define
+// to  serialize user object
+// user object store serialized form for furthur use
+passport.serializeUser((user, done) => {
+    // deserialize user;
+    // deserialize 
+    done(null, user.gmail_id);
+})
+// whenver some user send cookie 
+passport.deserializeUser(async (gmail_id, done) => {
+    console.log(id);
+    let resArr = await query(`SELECT * from user WHERE gmail_id="${gmail_id}"`);
+    if (resArr.length == 0) {
+        done(null, "user not found")
+    } else {
+        done(null, resArr[0])
+    }
+})
+
 passport.use(new GoogleStrategy({
+    clientID: "442880052776-f6vbup6kn2aa46b1ucr1s7feac3l6ioo.apps.googleusercontent.com",
+    clientSecret: "PfloDXkHhqD4e2eNMQfeDAcu",
     callbackURL: "http://localhost:4000/auth/callback"
     // passReqToCallback: true
 },
-    function (request, accessToken, refreshToken, profile, done) {
+    async function (request, accessToken, refreshToken, profile, done) {
         console.log("Inside passport cb")
         console.log(profile);
-        // signup
-        // login
+        let resArr = await query(`SELECT * from user WHERE gmail_id="${profile.id}"`);
+        let user = {};
+        if (resArr.length == 0) {
+            // signup
+            user = {
+                gmail_id: profile.id,
+                p_img_url: profile.picture,
+                email_id: profile.email,
+                id: uuidv4()
+            }
+            await query(`INSERT INTO user SET?`, user);
+
+        } else {
+            // login
+            user = resArr[0];
+        }
+        // google-> recieved our user , signup/login 
+        // res send client=> browser
+        done(null, user);
+        // done(null, user);
     }
 ));
+
 // req to google server => to get email  and profile
 // passport .authectiecte => function google [email,profile],/setting
 // redirect to googl server
@@ -29,7 +92,15 @@ app.get("/auth/google",
 app.get("/auth/callback", passport.authenticate("google"), function (req, res) {
     //identify 
     // req using passport.authenticate
-    res.send("user authenticated");
+    console.log(req.user);
+    // res.send("user authenticated");
+    res.redirect("/private");
+})
+app.get("/private", function (req, res) {
+    res.json({
+        user:req.user,
+        status:"successfull"
+    })
 })
 app.listen(4000,
     console.log("Server is listening at port 4000"));
